8d668785d10bbecf7d761ce6582ae2df092bc8ed
Aizo Kroon <me@aizokroon.nl>
2014-06-04 12:20:54 +0200

    Deleted the test class and added the Vehicle specification

---
+++ src/Vehicle.java
@@ -0,0 +1,14 @@
+public class Vehicle {
+	// - cost : int
+	// - speed : int
+	
+	// Pre: c > 0 and s > 0.
+	// Post: has created a Vehicle with cost c and speed s.
+	// + Vehicle(c : int, s : int)
+	
+	// Post: returns cost.
+	// + getCost() : int
+	
+	// Post:  returns speed.
+	// + getSpeed() :int
+}
