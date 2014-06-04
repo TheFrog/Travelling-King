e1bfc49de9ae1cab10ad287571a58dd75de5dda4
Aizo Kroon <me@aizokroon.nl>
2014-06-04 14:47:45 +0200

    Added name to Vehicle class

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,13 +1,17 @@
 public class Vehicle {
 	// - cost : int
+	// - name : String
 	// - speed : int
 	
 	// Pre: c > 0 and s > 0.
 	// Post: has created a Vehicle with cost c and speed s.
-	// + Vehicle(c : int, s : int)
+	// + Vehicle(c : int, n : String, s : int)
 	
 	// Post: returns cost.
 	// + getCost() : int
+	
+	// Post: returns name.
+	// + getName() : String
 	
 	// Post:  returns speed.
 	// + getSpeed() :int
