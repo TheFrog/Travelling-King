c997c45b2c39d1fb2a50585ed7a0cce835a731fc
Friso Kingma <fhkingma@gmail.com>
2014-06-18 11:03:44 +0200

    Implemented Edge, Node and Graph (partially)

--- src/Edge.java
+++ src/Edge.java
@@ -2,17 +2,29 @@
 public class Edge {
 	// The length of the Edge.
 	// - length : float
+	private float length;
 	
 	// The node at the end of the Edge.
 	// - target : Node
+	private Node target;
 	
 	// Pre: l >= 0 and t != null.
 	// Post: has created an Edge with length l and target t.
 	// + Edge(l : float, t : Node)
+	public Edge(float l, Node t){
+		length = l;
+		target = t;
+	}
 
 	// Post: return length.
 	// + getLength() : float
+	public float getLength(){
+		return this.length;
+	}
 	
 	// Post: returns target.
 	// + getTarget() : Node
+	public Node getTarget(){
+		return target;
+	}
 }
--- src/Graph.java
+++ src/Graph.java
@@ -2,15 +2,37 @@
 public class Graph {
 	// Post: has created an empty graph.
 	// + Graph()
+	public Graph() {
+		
+	}
 	
 	// Pre: source != null and target != null.
 	// Post: has added an edge from Node source to Node target with the specified length, and returns this Edge.
 	// + addEdge(source : Node, target : Node, length : float) : Edge
+	public Edge addEdge(Node source, Node target, float length) {
+		if(source == null && target == null){
+			return null;
+		}
+		
+		Edge e = new Edge(length, target);
+		source.addEdge(e);
+		return e;
+	}
 	
 	// Post: return a newly created (unconnected) Node.
 	// + addNode() : Node
+	public Node addNode() {
+		Node n = new Node();
+		return n;
+	}
 	
 	// Pre: source != null and target != null.
 	// Post: returns the shortest path from source to target, and throws a NodesNotConnectedException if no path is found (so the Nodes are not connected).
 	// + shortestPath(source : Node, target : Node) : float
+	public float shortestPath(Node source, Node target) {
+		if(source == null || target == null){
+			return null;
+		}
+		//DIJKSTRA
+	}
 }
