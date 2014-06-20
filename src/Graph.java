3178bfd41c8a560bc9c1716825a4b66f4f600f6c
Aizo Kroon <me@aizokroon.nl>
2014-06-20 11:27:17 +0200

    Implemented Dijktra

--- src/Graph.java
+++ src/Graph.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 // Represents a Graph with Nodes connected by Edges.
 public class Graph {
 	// Post: has created an empty Graph.
@@ -10,7 +12,7 @@ public class Graph {
 	// Post: has added an edge from Node source to Node target with the specified length, and returns this Edge.
 	// + addEdge(source : Node, target : Node, length : float) : Edge
 	public Edge addEdge(Node source, Node target, float length) {
-		if(source == null && target == null){
+		if(source == null && target == null) {
 			return null;
 		}
 		
@@ -27,12 +29,52 @@ public class Graph {
 	}
 	
 	// Pre: source != null and target != null.
-	// Post: returns the shortest path from source to target, and throws a NodesNotConnectedException if no path is found (so the Nodes are not connected). 
+	// Post: returns the shortest path from source to target, and throws a NodesNotConnectedException if no path is found (so the Nodes are not connected).Throws an IllegalArgumentException if the pre-conditions are not met. 
 	// + shortestPath(source : Node, target : Node) : float
-	public float shortestPath(Node source, Node target) {
-		if(source == null || target == null){
+	public float shortestPath(Node source, Node target) throws IllegalArgumentException, NodesNotConnectedException {
+		if (source == null || target == null) {
 			throw new IllegalArgumentException();
 		}
 		
+		// Store Nodes that have to be visited.
+		Queue<Node> queue = new PriorityQueue<Node>();
+		
+		// Store distance from the source for every Node.
+		Map<Node, Float> distances = new HashMap<Node, Float>();
+		
+		// Add the source Node.
+		distances.put(source, new Float(0));
+		queue.add(source);
+		
+		while (!queue.isEmpty()) {
+			Node n = queue.poll();
+			List<Edge> edges = n.listEdges();
+			
+			for (Edge e : edges) {
+				Node t = e.getTarget();
+				
+				// Get the current distance to Node t.
+				if (distances.get(t) == null) {
+					distances.put(t, Float.POSITIVE_INFINITY);
+				}
+				float d = distances.get(t);
+				
+				// Get the distance through Node n to Node t.
+				float distanceThroughN = distances.get(n) + e.getLength();
+				
+				// Update the distances.
+				if (distanceThroughN < d) {
+					queue.remove(t);
+					distances.put(t, distanceThroughN);
+					queue.add(t);
+				}
+			}
+		}
+		
+		// Throw a NodesNotConnectedException if Node target was not reached.
+		if (distances.get(target) == null) {
+			throw new NodesNotConnectedException();
+		}
+		return distances.get(target);
 	}
 }
---
+++ src/NodesNotConnectedException.java
@@ -0,0 +1,17 @@
+public class NodesNotConnectedException extends Exception {
+	public NodesNotConnectedException() {
+		super();
+	}
+
+	public NodesNotConnectedException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public NodesNotConnectedException(String message) {
+		super(message);
+	}
+
+	public NodesNotConnectedException(Throwable cause) {
+		super(cause);
+	}
+}
